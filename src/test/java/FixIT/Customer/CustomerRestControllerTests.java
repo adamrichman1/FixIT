c7da5d80f12c505c7692299510b74d95d892e7f5
Pyrantix <adam.richman@pyrantix.org>
2019-02-12 19:56:58 -0500

    added test files

---
+++ src/test/java/FixIT/Customer/CustomerRestControllerTests.java
@@ -0,0 +1,23 @@
+package FixIT.Customer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CustomerRestControllerTests {
+
+    @Before
+    public void setup() {
+        // TODO
+    }
+
+    @After
+    public void teardown() {
+        // TODO
+    }
+
+    @Test
+    public void testLogin() {
+        // TODO
+    }
+}
---
+++ src/test/java/FixIT/Staff/StaffRestControllerTests.java
@@ -0,0 +1,23 @@
+package FixIT.Staff;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StaffRestControllerTests {
+
+    @Before
+    public void setup() {
+        // TODO
+    }
+
+    @After
+    public void teardown() {
+        // TODO
+    }
+
+    @Test
+    public void testLogin() {
+        // TODO
+    }
+}
