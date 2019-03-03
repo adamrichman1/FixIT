8d5f4c0064cbdde309f7ba584257021317c536f0
Adam Richman <adamsrichman1@gmail.com>
2019-03-03 18:01:42 -0500

    added DBManager

--- src/main/java/FixIT/Core/DBManager.java
+++ src/main/java/FixIT/Core/DBManager.java
@@ -1,73 +1,187 @@
 package FixIT.Core;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.sql.*;
 
 /**
- * This class manages core database operations, including connect and closeConnection. Subclasses use these methods to
- * execute DB operations.
+ * This class controls core DB operations, such as creating/dropping tables and connecting/closing DB connections
  */
 public abstract class DBManager {
-    private static String dbURL = "";
+
+    private static Logger logger = LoggerFactory.getLogger(DBManager.class);
+    private static String dbURL;
 
     /**
-     * Used by subclasses to connect to the DB before executing SQL
+     * Used to initialize DBManager resources
      *
      * @param dbURL the URL of the DB to connect to
-     * @return a Connection object that allows DB queries
      */
-    protected Connection connect(String dbURL) {
-        // TODO
+    public static void initialize(String dbURL) {
+        DBManager.dbURL = dbURL;
+    }
+
+    /**
+     * Used to connect to the DB
+     *
+     * @return a Connection object for the DB
+     */
+    private static Connection connect(String dbURL) {
+        Connection c = null;
+        try {
+            if (dbURL != null) c = DriverManager.getConnection(dbURL);
+            else throw new IllegalStateException();
+
+        } catch (SQLException e) {
+            logger.error(e.getMessage(), e);
+        }
+        if(c == null){
+            logger.error(">>> ERROR: System failed to connect to DB");
+            System.exit(1);
+        }
+        return c;
+    }
+
+    /**
+     * Used to close a DB connection
+     *
+     * @param c the Connection object to close
+     */
+    private static void close(Connection c) {
+        try {
+            c.close();
+        } catch(SQLException e) {
+            logger.error(">>> ERROR: Couldn't close DB connection object");
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Used to drop a table
+     *
+     * @param table the table to drop
+     */
+    static void dropTable(String table){
+        String sql = "DROP TABLE IF EXISTS " + table;
+        executeUpdate(sql);
+    }
+
+    /**
+     * Used to execute an SQL query
+     *
+     * @param sql the query string
+     * @param queryParams parameters for the query specific in sql
+     * @return the ResultSet containing data from the query
+     */
+    static ResultSet executeQuery(String sql, Object... queryParams) {
+        Connection conn = connect(dbURL);
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement = prepareStatement(statement, queryParams);
+            ResultSet resultSet = statement.executeQuery();
+            close(conn);
+            return resultSet;
+        } catch (SQLException e) {
+            logger.error(">>> ERROR: Couldn't execute query <<<", e);
+            close(conn);
+            System.exit(1);
+            return null;
+        }
+    }
+
+    /**
+     * Used to convert a ResultSet object into a specific class
+     *
+     * @param rs the ResultSet object containing match data from the query
+     * @param id the id of the column to extract from the ResultSet
+     * @param c the Class to convert the ResultSet column to
+     * @return an array of Team objects containing team data from the query
+     */
+    static <S> S deserializeResultSetCol(ResultSet rs, String id, Class<S> c) {
+        try {
+            if (rs.next()) {
+                return deserializeString(rs.getString(id), c);
+            }
+        } catch (SQLException e) {
+            logger.error(">>> ERROR: Couldn't extract ResultSet column " + id, e);
+            System.exit(1);
+        }
         return null;
     }
 
     /**
-     * Used by subclasses to close Connection objects once a query is complete
+     * Used to deserialize an object
      *
-     * @param c the connection object to close
+     * @param data the data to deserialize
+     * @param c the class of generic type S that the data will be deserialized into
+     * @param <S> the generic type that the data will be deserialized into
+     * @return the deserialized form of the data provided
      */
-    protected void closeConnection(Connection c) {
-        // TODO
-    }
-
-    /**
-     * Used by subclasses to create specific types of tables (e.g. appointment table)
-     *
-     * @param dbURL the URL of the DB to connect to
-     * @param sql the SQL string containing a query to create a table
-     */
-    protected void createTable(String dbURL, String sql) {
-        // TODO
-    }
-
-    /**
-     * Used to drop (delete) a table, for development purposes
-     *
-     * @param dbURL the URL of the DB to connect to
-     * @param tableName the name of the table to drop
-     */
-    protected void dropTable(String dbURL, String tableName) {
-        // TODO
-    }
-
-    /**
-     * Used by subclasses to execute queries
-     *
-     * @param sql the SQL string containing the query to execute
-     * @param queryParams an array of objects containing the query parameters to prepare the SQL statement with
-     */
-    protected ResultSet executeQuery(String sql, Object[] queryParams) {
-        // TODO
+    static <S> S deserializeString(String data, Class<S> c) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(data, c);
+        } catch (IOException e) {
+            logger.error(">>> ERROR: Couldn't deserialize data into type " + c.getName(), e);
+            System.exit(1);
+        }
         return null;
     }
 
     /**
-     * Used by subclasses to execute update queries
+     * Used to execute an SQL update query
      *
-     * @param sql the SQL string containing the update query to execute
-     * @param queryParams an array of objects containing the query parameters to prepare the SQL statement with
+     * @param sql the query string to execute
+     * @param queryParams parameters for the query specific in sql
+     * @return the ResultSet of the update
      */
-    protected void executeUpdateQuery(String sql, Object[] queryParams) {
-        // TODO
+    static ResultSet executeUpdate(String sql, Object... queryParams) {
+        Connection conn = connect(dbURL);
+        try {
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement = prepareStatement(statement, queryParams);
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            close(conn);
+            return rs;
+        } catch(SQLException e){
+            logger.error(">>> ERROR: Couldn't execute update query", e);
+            close(conn);
+            System.exit(1);
+            return null;
+        }
+    }
+
+    /**
+     * Used to prepare a statement before executing a query
+     *
+     * @param statement the query statement to prepare with query parameters
+     * @param queryParams the parameters to prepare the statement with
+     * @return the prepared statement
+     */
+    private static PreparedStatement prepareStatement(PreparedStatement statement, Object... queryParams) {
+        try {
+            int i = 1;
+            for (Object queryParam: queryParams) {
+                statement.setObject(i++, queryParam);
+            }
+            return statement;
+        } catch (SQLException e) {
+            logger.error(">>> ERROR: Couldn't prepare statement", e);
+            System.exit(1);
+            return null;
+        }
+    }
+
+    /**
+     * Package-private getters for class variables - helps with testing
+     * @return class variables
+     */
+    String getDbURL(){
+        return dbURL;
     }
 }
