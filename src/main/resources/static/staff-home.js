bd5d21fd78de43678a1a7a8f6d366adecf11a3e8
Ben Rarrick <benrarrick@aol.com>
2019-04-01 13:31:39 -0400

    Polished up looks and added an example test data

--- src/main/resources/static/staff-home.js
+++ src/main/resources/static/staff-home.js
@@ -4,6 +4,7 @@ window.addEventListener("load",initialize,true);
 function initialize(){
 
 
+
     //initial call to poll staff appointments
     pollAppointments();
 
@@ -14,7 +15,7 @@ function initialize(){
 
     //poll the model for any new rooms that may have been created
     function pollAppointments() {
-        var xmlHttp = new XMLHttpRequest();
+       /* var xmlHttp = new XMLHttpRequest();
         xmlHttp.onreadystatechange = function() {
             if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
                 var rawJson = JSON.parse(xmlHttp.responseText);
@@ -22,32 +23,44 @@ function initialize(){
             }
         };
         xmlHttp.open("GET", '/staff/getAppointments/', true);
-        xmlHttp.send(null);
+        xmlHttp.send(null);*/
+        var testData = {"appointmentTime": 1000,"notes":"FixComputer","date":"4/28/19","ID":1,"name":"John"};
+        updateChatRoomList(testData, pollAppointments);
     }
 
 
     //update the DOM to show the updated list of appointments
     function updateChatRoomList(rawJson, callback){
-        if (allApps.length !== 0){
+        if (true){
 
             var staffAppHead = document.getElementById('staffAppHead');
             staffAppHead.textContent = 'Currently Scheduled Appointments:';
 
             var staffAppList = document.getElementById('staffAppList');
             staffAppList.innerHTML = '';
-            for(var i = 0; i < allApps.length; i++){
+            for(var i = 0; i < 1; i++){
                 var appInd = document.createElement('li');
-                var appmJoin = document.createElement('a');
+                var appTime = document.createElement('h4');
+                var appDate = document.createElement('h4');
+                var appName = document.createElement('h4');
+                var appNotesHead = document.createElement('h4');
+                var appNotes = document.createElement('p');
                 var appDel = document.createElement('button');
-                appDel.id = '/deleteRoom/'+allApps[i].id;
-                appDel.addEventListener('click', deleteRoom,false);
-                appInd.textContent = allApps[i].name+ " ";
-                appJoin.textContent = "Join Chat";
-                appJoin.href = '/room/'+allApps[i].id;
-                appDel.textContent = "Delete Room";
-                appDel.id = '/deleteRoom/'+allApps[i].id;
+                appDel.id = rawJson.ID;
+                appDel.addEventListener('click', cancelAppointment,false);
+                appTime.textContent = "Appointment Time: "+ rawJson.appointmentTime;
+                appDate.textContent = "Appointment Date: "+rawJson.date;
+                appName.textContent = "Customer Name: "+rawJson.name;
+                appNotesHead.textContent = "Appointment Notes:";
+                appNotes.textContent = rawJson.notes;
+                appDel.textContent = "Cancel Appointment";
+
                 staffAppList.appendChild(appInd);
-                appInd.appendChild(appJoin);
+                appInd.appendChild(appTime);
+                appInd.appendChild(appDate);
+                appInd.appendChild(appName);
+                appInd.appendChild(appNotesHead);
+                appInd.appendChild(appNotes);
                 appInd.appendChild(appDel);
             }
         }
@@ -58,3 +71,18 @@ function initialize(){
 
         setTimeout(callback, 1000);
     }
+
+    //function to cancel an appointment
+    function cancelAppointment(eve){
+        var appointmentID = eve.srcElement.id;
+        /* var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() {
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+                var rawJson = JSON.parse(xmlHttp.responseText);
+                //updateChatRoomList(rawJson, pollAppointments);
+            }
+        };
+        xmlHttp.open("POST", '/staff/cancelAppointment/'+appointmentID, true);
+        xmlHttp.send(null);*/
+    }
+
