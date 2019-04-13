1b27bb9ed03ffc477b7e57acafd90b35207516b3
Josh Gulden <jdg78@pitt.edu>
2019-04-13 19:46:25 -0400

    updating staff appointment views

--- src/main/resources/static/js/staff-home.js
+++ src/main/resources/static/js/staff-home.js
@@ -1,98 +1,111 @@
 window.addEventListener("load",initialize,true);
 
+const PENDING = 0;
 
 function initialize(){
-
-
     //initial call to poll staff appointments
     pollAppointments();
 
 }
 
-
-    //poll the model for any new rooms that may have been created
-    function pollAppointments() {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-                var rawJson = JSON.parse(xmlHttp.responseText);
-                console.log(rawJson);
-                updateChatRoomList(rawJson, pollAppointments);
-            }
-            else{
-                console.log("error: "+ xmlHttp.status);
-            }
-        };
-        xmlHttp.open("GET", '/staff/getAppointmentHistory', true);
-        xmlHttp.send(null);
-    }
-
-
-    //update the DOM to show the updated list of appointments
-    function updateAppointmentList(rawJson, callback){
-
-        //TODO if there is an appointment
-        if (true){
-
-
-            var staffAppHead = document.getElementById('staffAppHead');
-            staffAppHead.textContent = 'Currently Scheduled Appointments:';
-
-            var staffAppList = document.getElementById('staffAppList');
-            staffAppList.innerHTML = '';
-
-            // TODO how to iterate through list
-            for(var i = 0; i < 1; i++){
-                var appInd = document.createElement('li');
-                var appTime = document.createElement('h4');
-                var appDate = document.createElement('h4');
-                var appAddress = document.createElement('h4');
-                var appSubject = document.createElement('h4');
-                var appName = document.createElement('h4');
-                var appNotesHead = document.createElement('h4');
-                var appNotes = document.createElement('p');
-                var appDel = document.createElement('button');
-                appDel.id = rawJson.ID;
-                appDel.addEventListener('click', cancelAppointment,false);
-                appTime.textContent = "Appointment Time: "+ rawJson.appointmentTime;
-                appDate.textContent = "Appointment Date: "+rawJson.date;
-                appAddress.textContent = "Appointment Address: "+rawJson.address;
-                appSubject.textContent = "Appointment Subject: "+rawJson.subject;
-                appName.textContent = "Customer Name: "+rawJson.name;
-                appNotesHead.textContent = "Appointment Notes:";
-                appNotes.textContent = rawJson.notes;
-                appDel.textContent = "Cancel Appointment";
-
-                staffAppList.appendChild(appInd);
-                appInd.appendChild(appDate);
-                appInd.appendChild(appTime);
-                appInd.appendChild(appAddress);
-                appInd.appendChild(appSubject);
-                appInd.appendChild(appNotesHead);
-                appInd.appendChild(appNotes);
-                appInd.appendChild(appDel);
-                staffAppList.appendChild(appInd);
-            }
+function pollAppointments() {
+    $.ajax({
+        url: "http://localhost:8080/staff/getAppointmentHistory",
+        type: 'GET',
+        contentType: "application/json",
+        headers: {
+            'username': localStorage.getItem("username")
+        },
+        success: function(data) {
+            console.log("Hello");
+            console.log(data);
+            populateAppointments(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log(jqXHR.status);
+            console.log(textStatus);
+            console.log(errorThrown);
+            // TODO - ERROR HANDLE
         }
-        else{
-            var staffAppHead = document.getElementById('staffAppHead');
-            staffAppHead.textContent = 'You currently have no appointments scheduled';
-        }
+    });
+}
 
-        setTimeout(callback, 1000);
-    }
+function populateAppointments(appointments) {
+    if (appointments.length !== 0) {
+        let apptHead = document.getElementById('appt-head');
+        apptHead.textContent = 'Currently Scheduled Appointments';
 
-    //function to cancel an appointment
-    function cancelAppointment(eve){
-        var appointmentID = eve.srcElement.id;
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {
-            if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
-                var rawJson = JSON.parse(xmlHttp.responseText);
-                //updateChatRoomList(rawJson, pollAppointments);
+        let apptList = document.getElementById('appt-list');
+
+        for (var i = 0; i < appointments.length; i++) {
+            const appointmentID = appointments[i].appointmentID;
+
+            let listItem = document.createElement('li');
+            listItem.className = "list-group-item";
+
+            let apptData = document.createElement('div');
+
+            let idLink = document.createElement('a');
+            idLink.innerText = "ID: " + appointments[i].appointmentID;
+
+            let problem = document.createElement('p');
+            problem.className = 'small';
+            problem.innerText = "Description: " + appointments[i].problem;
+
+            let apptTime = document.createElement('p');
+            apptTime.className = 'small';
+            apptTime.innerText = "Time: " + appointments[i].appointmentTime;
+
+            let acceptButton = document.createElement('button');
+
+            acceptButton.className = ' btn btn-primary';
+            acceptButton.innerText = "Accept Appointment";
+
+            acceptButton.addEventListener('click', function() {
+                const formData = {
+                    "appointmentID": appointmentID,
+                    "appointmentStatus": 1
+                };
+                updateAppointmentStatus(formData);
+            });
+
+            if (appointments[i].appointmentStatus === PENDING) {
+                acceptButton.type = hidden;
             }
-        };
-        xmlHttp.open("POST", '/staff/cancelAppointment/'+appointmentID, true);
-        xmlHttp.send(null);
-    }
 
+            apptData.append(idLink);
+            apptData.append(problem);
+            apptData.append(apptTime);
+            apptData.append(acceptButton);
+
+            listItem.appendChild(apptData);
+
+            apptList.appendChild(listItem);
+        }
+    } else {
+        let apptHead = document.getElementById('appt-head');
+        apptHead.textContent = 'No Appointments';
+    }
+}
+
+function updateAppointmentStatus(formData) {
+    $.ajax({
+        url: "http://localhost:8080/updateAppointmentStatus",
+        type: 'PUT',
+        contentType: "application/json",
+        headers: {
+            'username': localStorage.getItem("username")
+        },
+        data: JSON.stringify(formData),
+        success: function() {
+            console.log(formData);
+            console.log("Hello");
+        },
+        error: function(jqXHR, textStatus, errorThrown){
+            console.log(jqXHR.status);
+            console.log(textStatus);
+            console.log(errorThrown);
+            // TODO - ERROR HANDLE
+        }
+    });
+}
