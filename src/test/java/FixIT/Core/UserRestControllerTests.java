d9911975c71af394237776190ec401ae73a0e188
Valerie Kristofic <valeriek0405@gmail.com>
2019-04-17 16:45:19 -0400

    more tests

--- src/test/java/FixIT/Core/UserRestControllerTests.java
+++ src/test/java/FixIT/Core/UserRestControllerTests.java
@@ -1,4 +1,65 @@
 package FixIT.Core;
 
+import FixIT.Staff.Staff;
+import FixIT.Staff.StaffDBManager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class UserRestControllerTests {
+    @Before
+    public void setup() {
+        StaffDBManager.getInstance().dropUserTable();
+        StaffDBManager.getInstance().createStaffTable();
+    }
+
+    @Test
+    public void testSignUpFormInvalidTrue(){
+        String username = "sohelsarwar";
+        String password = "123456";
+        String email = "sohel@sarwar.org";
+        String name = "sohel";
+        String address = "1530 Way, Pittsburgh PA";
+
+        Staff u = new Staff(username);
+
+        u.setPassword(password);
+        u.setEmail(email);
+        u.setName(name);
+        u.setAddress(address);
+
+
+        // Test
+        boolean result = true;
+        // result =UserRestController.getInstance().signUpFormInvalid(u)
+
+        //Assert
+        assertEquals(true, result);
+    }
+
+    @Test
+    public void testSignUpFormInvalidFalse(){
+        String username = "sohelsarwar";
+        String password = "123456";
+        String email = "";
+        String name = "sohel";
+        String address = "1530 Way, Pittsburgh PA";
+
+        Staff u = new Staff(username);
+
+        u.setPassword(password);
+        u.setEmail(email);
+        u.setName(name);
+        u.setAddress(address);
+
+
+        // Test
+        boolean result = false;
+        // result =UserRestController.getInstance().signUpFormInvalid(u)
+
+        //Assert
+        assertEquals(true, result);
+    }
 }
--- src/test/java/FixIT/Staff/StaffDBManagerTests.java
+++ src/test/java/FixIT/Staff/StaffDBManagerTests.java
@@ -3,6 +3,8 @@ package FixIT.Staff;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class StaffDBManagerTests {
 
     @Before
@@ -12,7 +14,40 @@ public class StaffDBManagerTests {
     }
 
     @Test
-    public void test() {
+    public void testInsertUserToDB() {
+        String username = "sohelsarwar";
+        Staff user = new Staff(username);
 
+        //Test
+        Staff s = StaffDBManager.getInstance().insertUserToDB(user);
+
+        //Asserts
+        assertEquals(s.getUsername(), username);
+    }
+
+    @Test
+    public void testStaffAvailable() {
+        String username = "sohelsarwar";
+        Staff user = new Staff(username);
+
+        //Test
+        Staff s = StaffDBManager.getInstance().insertUserToDB(user);
+        boolean result = StaffDBManager.getInstance().staffAvailable();
+
+        //Asserts
+        assertEquals(StaffDBManager.getInstance().staffAvailable(), result);
+    }
+
+    @Test
+    public void testFindStaff() {
+        String username = "sohelsarwar";
+        Staff user = new Staff(username);
+
+        //Test
+        Staff s = StaffDBManager.getInstance().insertUserToDB(user);
+        String staffMember = StaffDBManager.getInstance().findStaff();
+
+        //Asserts
+        assertEquals(StaffDBManager.getInstance().findStaff(), staffMember);
     }
 }
